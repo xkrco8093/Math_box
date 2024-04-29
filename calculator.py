56f4370f67c19ef1696180bfc8b4f18457fc5c3d
chaerin <xkrco8093@gmail.com>
2024-04-29 22:07:40 +0900

    Add title comment and Create meeting-log

--- calculator.py
+++ calculator.py
@@ -1,46 +1,6 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 1,
-   "id": "784f1b99-d11d-4c62-bfd0-88b0a92adfe1",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def add(a, b):\n",
-    "  return a+b\n",
-    "def subtract(a, b):\n",
-    "  return a-b"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "1b4f7d7b-9189-4e57-a316-b2d6d30fe200",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.3"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# 기본 계산기
+def add(a, b):
+  return a+b
+
+def subtract(a, b):
+  return a-b
